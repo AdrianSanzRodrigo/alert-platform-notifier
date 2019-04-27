599975ca3616ad045d70cfc82ed6f5d44d13b1c9
adrian.sanz <adrian.sanz@gruposantander.com>
2019-04-27 18:42:48 +0200

    Initial commit

---
+++ src/main/java/com/kschool/alertplatform/AlertPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kschool.alertplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertPlatformApplication.class, args);
+	}
+
+}
