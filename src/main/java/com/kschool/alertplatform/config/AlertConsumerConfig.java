1a818ec34d6d05f3bfb2460dc8a7d957f5b2fd7e
AdrianSanzRodrigo <adriansanzdata@gmail.com>
2019-05-04 18:21:14 +0200

    Add bootstrap address to retrieve value in consumer config

--- src/main/java/com/kschool/alertplatform/config/AlertConsumerConfig.java
+++ src/main/java/com/kschool/alertplatform/config/AlertConsumerConfig.java
@@ -2,6 +2,7 @@ package com.kschool.alertplatform.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -15,12 +16,16 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class AlertConsumerConfig {
+
+    @Value(value = "${kafka.bootstrap.address}")
+    private String bootstrapAddress;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                bootstrapAddress);
         props.put(
                 ConsumerConfig.GROUP_ID_CONFIG,
                 "alert-platform");
