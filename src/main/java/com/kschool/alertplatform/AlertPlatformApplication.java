e7cf2e97617560af746155dcb84a2680cfd3acab
AdrianSanzRodrigo <adriansanzdata@gmail.com>
2019-05-05 23:46:37 +0200

    Enable jwt authentication annotation

--- src/main/java/com/kschool/alertplatform/AlertPlatformApplication.java
+++ src/main/java/com/kschool/alertplatform/AlertPlatformApplication.java
@@ -1,10 +1,12 @@
 package com.kschool.alertplatform;
 
+import com.kschool.alertplatform.security.annotations.EnableJwtAuthentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties
+@EnableJwtAuthentication
 @SpringBootApplication
 public class AlertPlatformApplication {
 	public static void main(String[] args) {
