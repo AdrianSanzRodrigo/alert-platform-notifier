709a35fec8f1b18e9eb1e71b6e5070ce891bc25f
AdrianSanzRodrigo <adriansanzdata@gmail.com>
2019-05-06 00:37:57 +0200

    Added main

--- alert-configuration/src/test/java/com/kschool/alertconfiguration/AlertConfigurationApplicationTests.java
+++ alert-configuration/src/test/java/com/kschool/alertconfiguration/AlertConfigurationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kschool.alertplatform;
+package com.kschool.alertconfiguration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
---
+++ alert-streaming/src/main/java/com/kschool/alertstreaming/AlertStreamingApplication.java
@@ -0,0 +1,4 @@
+package com.kschool.alertstreaming;
+
+public class AlertStreamingApplication {
+}
